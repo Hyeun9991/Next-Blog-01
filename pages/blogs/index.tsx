9edad6a356cfb6ae18839a24de3e9ea638992706
Hyeun9991 <hyeun9991@gmail.com>
2023-03-24 11:28:10 +0900

    db에서 데이터를 받아와서 화면에 출력하는 로직 작성

--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -1,7 +1,31 @@
+import axios from 'axios';
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 import Layout from '../../components/layout';
 
+interface IPostData {
+  title: string;
+  body: string;
+  id: number;
+}
+
+// interface Props {
+//   posts: IPostData[];
+// }
+
 const Index = () => {
+  const [posts, setPosts] = useState<IPostData[]>([]);
+
+  const getPosts = () => {
+    axios.get('http://localhost:3001/posts').then((res) => {
+      setPosts(res.data);
+    });
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
     <Layout>
       <Head>
@@ -10,9 +34,14 @@ const Index = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className="text-2xl font-bold text-gray-900 mb-10">
-        포스트 리스트
-      </h1>
+      <section>
+        <h1 className="text-2xl font-bold text-gray-900 mb-10">
+          포스트 리스트
+        </h1>
+        {posts.map((post) => {
+          return <div key={post.id}>{post.title}</div>;
+        })}
+      </section>
     </Layout>
   );
 };
